3d0bcb1f5cc25eb95d90fd12d13cc82447cf9066
Addybuff <menanvardavronov@gmial.com>
2023-04-03 22:27:57 +0500

    New changes some parts doesnot work

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// Set the view engine to use Pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+const indexRouter = require('./routes/index');
+const studentsRouter = require('./routes/students');
+
+app.use('/', indexRouter);
+app.use('/students', studentsRouter);
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
